8ad5e1c276a9509b601808aaf6b5c80db9e9d735
Roger Pinheiro <roger.contato@outlook.com>
2022-02-14 11:31:13 -0300

    Exemplos da Aula 1

---
+++ type-number.js
@@ -0,0 +1,24 @@
+// Tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero - segundoNumero;
+
+console.log(operacaoMatematica)
+
+// ponto Flutuante
+
+const numeroPontoFlutunate = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutunate;
+
+
+
+// NaN -> A Number (Não é número)
+
+const alura = "Alura";
+console.log(alura * primeiroNumero)
---
+++ type-string.js
@@ -0,0 +1,13 @@
+const texto1 = "Olá, Mundo!";
+const texto2 = "Olá, Mundo!";
+const senha = "senhaSuperSegura456!";
+const stringDeNumeros = "1234";
+
+const citacao = "Meu nome é: ";
+const meuNome = "Roger"
+
+// concatenação (+)
+
+console.log(citacao + meuNome)
+
+// Template string OU template literal
