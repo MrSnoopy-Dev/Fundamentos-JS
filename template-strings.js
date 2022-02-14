1b39c111353e93e4c13b60f2706c197d5484982d
Roger Pinheiro <roger.contato@outlook.com>
2022-02-14 16:48:11 -0300

    Tipagem

---
+++ template-strings.js
@@ -0,0 +1,9 @@
+const nome = "Roger"
+const idade = 2021-1996
+const cidadeDeNascimento = "Itabuna-BA";
+
+// //const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
+
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+
+console.log(apresentacao)
