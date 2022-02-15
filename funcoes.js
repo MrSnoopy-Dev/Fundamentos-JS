d4088a60f1d1935b0e102a837d15d1c4cc215488
Roger Pinheiro <roger.contato@outlook.com>
2022-02-15 11:49:17 -0300

    Funções(Parâmetros x Argumentos)

---
+++ funcoes.js
@@ -0,0 +1,21 @@
+// let x = "";
+// console.log(x);
+// x = "oi";
+
+// 1) Declara a função 
+                        //string
+function imprimeTexto (texto) {
+  console.log(texto)
+}
+
+// // 2) Executa a função (1 ou + vezes)
+
+ imprimeTexto(soma())
+// imprimeTexto("Mundo")
+
+// Três Formas de Escrever funções
+
+function soma() {
+  return 2 + 2;
+}
+//console.log(soma())
---
+++ parametros.js
@@ -0,0 +1,27 @@
+//Parâmetro de função
+
+              //2         //2
+
+// console.log(soma(2, 2))
+// console.log(soma(245, 20))
+// console.log(soma(-500, 60))
+
+
+// Parâmetros x Argumentos
+
+//Ordem dos parâmetros
+
+
+function nomeIdade(nome, idade) {
+  return `Meu nome é ${nome} e minha idade é ${idade}`;
+}
+//console.log(nomeIdade(25, "Roger"))
+
+function soma(numero1, numero2) {
+  return numero1 + numero2;
+}
+function multiplica (numero1 = 1, numero2 = 1) {
+  return numero1 * numero2;
+}
+
+console.log(multiplica(soma(4, 5)))
